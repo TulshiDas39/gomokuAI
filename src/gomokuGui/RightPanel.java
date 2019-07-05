bcbbf59234529babcffbfed20dfb191ee349aae2
tulshi das <tulshidas37@gmail.com>
2019-07-06 01:32:32 +0600

    bug fixed for production

--- src/gomokuGui/RightPanel.java
+++ src/gomokuGui/RightPanel.java
@@ -4,7 +4,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
-import java.net.MalformedURLException;
+import java.io.IOException;
 import java.net.URL;
 
 import javax.swing.Icon;
@@ -13,27 +13,27 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class RightPanel extends JPanel {
-	
+
 	private static final long serialVersionUID = 1L;
 
 	Color colors[]= {Color.LIGHT_GRAY,Color.BLUE,Color.RED,Color.magenta,Color.ORANGE};
 	String position[] = {"1st","2nd","3rd","4th","5th","6th","7th","8th","9th","10th"};
-	
+
 	int turn;// turn=1 // turn=2 // turn= 3
 	JLabel labelGIF;
 	GomukuAI gomuku;
-	
+
 	boolean gameOver;
-	
+
 	JLabel turn1;
 	JLabel status;
-	
-	public RightPanel() throws MalformedURLException {
-		
+
+	public RightPanel() throws IOException {
+
 		setBackground(Color.BLACK);
 		setPreferredSize(new Dimension(445, getHeight()));
 		setLayout(new GridLayout(4, 1));
-		
+
 		turn1 = new JLabel("Your turn");
 		turn1.setForeground(Color.WHITE);
 		turn1.setFont(new Font("Sherif",Font.PLAIN,20));
@@ -42,36 +42,35 @@ public class RightPanel extends JPanel {
 		status.setHorizontalAlignment(getWidth());
 		status.setForeground(Color.PINK);
 		status.setFont(new Font("Sherif",Font.PLAIN,20));
-		
+
 		add(turn1);
 		add(status);
-		
-	
+
 	}
-	
-	public void addWinGIF() throws MalformedURLException {
-		String  path= System.getProperty("user.dir");
-		URL url = new URL("file:///"+path+"/congrats.gif");
-        	Icon icon = new ImageIcon(url);
-        	labelGIF = new JLabel(icon);
+
+	public void addWinGIF(){
+		URL congratsImageUrl = getClass().getResource("/images/congrats.gif");
+		Icon icon = new ImageIcon(congratsImageUrl);
+		labelGIF = new JLabel(icon);
 		add(labelGIF);
-        
+
 	}
-	
+
 	/*public void viewCongrats(){
 		labelGIF.setVisible(true);
-		
+
 	}*/
-	
-	public void addLoseGIF() throws MalformedURLException{
-		 String  path= System.getProperty("user.dir");
-		URL url = new URL("file:///"+path+"/game_over.gif");
+
+	public void addLoseGIF(){
+		//		String  path= System.getProperty("user.dir");
+		//		URL url = new URL("file:///"+path+"/game_over.gif");
+		URL url = getClass().getResource("/images/game_over.gif");
 		Icon icon = new ImageIcon(url);
 		labelGIF = new JLabel(icon);
 		labelGIF.setPreferredSize(new Dimension(getWidth(), 50));
 		add(labelGIF);
 	}
-	
+
 
 	public void changeTurn(int row , int col,boolean isWin){
 		if(row == -1 && col == -1)turn1.setText("Computer is thinking");
@@ -80,19 +79,19 @@ public class RightPanel extends JPanel {
 		}
 		else turn1.setText("<html>Computer maked move in "+position[row]+" row,"+position[col]+" column.<br/>Now its your turn</html>");
 	}
-	
+
 	public void viewWinner(String status){
 		this.status.setText(status);
 		this.status.setFont(new Font("Sherif",Font.BOLD,25));
 		this.status.setForeground(Color.PINK);
 	}
-	
-	
-	
+
+
+
 }
-		
-		
-		
-	
-	
-	
+
+
+
+
+
+
--- src/gomokuGui/Window.java
+++ src/gomokuGui/Window.java
@@ -2,20 +2,21 @@ package gomokuGui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Image;
-import java.awt.Label;
-import java.io.File;
 import java.io.IOException;
-
+import java.net.URL;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-
 public class Window extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public Window() throws IOException {
 		setLayout(new BorderLayout());
 		RightPanel rPanel = new RightPanel();
@@ -29,7 +30,9 @@ public class Window extends JFrame {
 		label.setFont(new Font("Sherif",Font.BOLD,55));
 		label.setHorizontalAlignment(getWidth());
 		add(label,BorderLayout.NORTH);
-		ImageIcon image = new ImageIcon(ImageIO.read(new File("forLeft.jpg")).getScaledInstance(300, 680, Image.SCALE_SMOOTH));
+		//ImageIcon image = new ImageIcon(ImageIO.read(new File("forLeft.jpg")).getScaledInstance(300, 680, Image.SCALE_SMOOTH));
+		URL leftImageUrl = getClass().getResource("/images/forLeft.jpg");
+		ImageIcon image = new ImageIcon(ImageIO.read(leftImageUrl).getScaledInstance(300, 680, Image.SCALE_SMOOTH));
 		
 		add(new JLabel(image),BorderLayout.WEST);
 		
